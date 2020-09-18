bed96e8949c0928a988848482a55b1aa70aafbe2
tinafang-1207 <yutian.fang@duke.edu>
2020-09-17 20:56:36 -0400

    Initial commit of ARGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Yutian Fang (yutian.fang@duke.edu)
+# Date:   Fall 2020
+#------------------------------------------------------------
+
