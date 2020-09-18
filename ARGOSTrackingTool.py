9261f722abc29b8f4ac1358efb03f9db7342a9ec
tinafang-1207 <yutian.fang@duke.edu>
2020-09-18 10:24:49 -0400

    Reads data in from file

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,3 +9,31 @@
 # Date:   Fall 2020
 #------------------------------------------------------------
 
+#Create a variable pointing to the data file
+file_name = './Data/Raw/Sara.txt'
+
+#Create a file object from the file
+file_object = open(file_name, 'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+lineString = line_list[2]
+
+#Split the string into a list of data items
+lineData = lineString.split()
+
+#Extract items in list into variables
+record_id = lineData[0]
+obs_data = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
+
+#print the location of Sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
